4e8305d33ef1c46325856f49912863e14b6c11e3
Frentsel Alexandr <frentsel3@gmail.com>
2018-11-26 07:51:20 +0200

    Update tpl.js

--- tpl.js
+++ tpl.js
@@ -1,7 +1,17 @@
 const tpl = function(str, data) {
   const getValue = (obj, keys) => {
-    if (typeof obj !== 'object') return obj;
-    if (keys) return getValue(obj[keys.shift()], keys);
+    if (typeof obj !== 'object') {
+      return obj;
+    }
+    if (keys) {
+      const key = keys.shift();
+      const data = obj[key];
+      return getValue(data, keys);
+    }
+    return obj.toString();
   }
-  return str.replace(/\{([^\}]+)\}/g, (_, keys) => getValue(data, keys.split('.')));
+  return str.replace(/\{([^\}]+)\}/g, (idle, keys) => {
+    const keysList = keys.split('.');
+    return getValue(data, keysList);
+  });
 };
